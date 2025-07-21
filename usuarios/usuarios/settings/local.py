2e48fb1968381d840ab1f44747ba3e78d14ea6fd
Santiago <santiagomendieta2698@gmail.com>
2025-07-21 09:14:18 -0500

    Primer commit del proyecto desde PyCharm

---
+++ usuarios/usuarios/settings/local.py
@@ -0,0 +1,31 @@
+import os
+
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbuser',
+        'USER': 'santiago',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIR = [BASE_DIR.child('static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = [BASE_DIR.child('media')]
