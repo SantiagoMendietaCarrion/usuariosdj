6b4a7a082c590bb52d6dd5e46c056d5ac7277981
Santiago <santiagomendieta2698@gmail.com>
2025-07-21 10:05:19 -0500

    Segundo commit del proyecto

--- usuarios/usuarios/settings/base.py
+++ usuarios/usuarios/settings/base.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+from django.core.exceptions import ImproperlyConfigured
+import json
 
 #from pathlib import Path
 from unipath import Path
@@ -20,7 +22,17 @@ BASE_DIR = Path(__file__).ancestor(3)
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7vwk-$nmlwb*j)scfy#qn3806turlm@=%axxa)f=7t5w^hq-%r'
+with open("secret.json") as f:
+    secret = json.loads(f.read())
+
+def get_secret(secret_name, secrets=secret):
+    try:
+        return secrets[secret_name]
+    except:
+        msg = "La variable %s no existe" % secret_name
+        raise ImproperlyConfigured(msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # Application definition
 
--- usuarios/usuarios/settings/local.py
+++ usuarios/usuarios/settings/local.py
@@ -13,9 +13,9 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dbuser',
-        'USER': 'santiago',
-        'PASSWORD': '123',
+        'NAME': get_secret("DB_NAME"),
+        'USER': get_secret("USER"),
+        'PASSWORD': get_secret("PASSWORD"),
         'HOST': 'localhost',
         'PORT': '5432',
     }
